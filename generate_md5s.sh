82a7b68cfb9a32c65d0c86cfaa5f757e8a90433b
h8d13 <hadrien.loge@gmail.com>
2025-11-07 11:39:52 +0100

    init script

---
+++ generate_md5s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Generate MD5 hashes for all tracked files
+
+git ls-files | grep -v "^\.md5/" | while read file; do
+    if [ -f "$file" ]; then
+        mkdir -p ".md5/$(dirname "$file")"
+        md5sum "$file" | awk '{print $1}' > ".md5/$file.md5"
+        echo "Generated: .md5/$file.md5"
+    fi
+done
+
+echo "Done! Now commit the .md5/ directory"
